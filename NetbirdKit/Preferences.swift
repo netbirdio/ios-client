f9bea1912aaa4427fb61441a32af389eb68e3998
Pascal Fischer <32096965+pascal-fischer@users.noreply.github.com>
2024-12-06 16:50:13 +0100

    Route states support (#19)

--- NetbirdKit/Preferences.swift
+++ NetbirdKit/Preferences.swift
@@ -10,13 +10,20 @@ import NetBirdSDK
 
 class Preferences {
     static func newPreferences() -> NetBirdSDKPreferences {
-        return NetBirdSDKNewPreferences(configFile())!
+        return NetBirdSDKNewPreferences(configFile(), stateFile())!
     }
 
     static func configFile() -> String {
         let fileManager = FileManager.default
         let groupURL = fileManager.containerURL(forSecurityApplicationGroupIdentifier: "group.io.netbird.app")
         let logURL = groupURL?.appendingPathComponent("netbird.cfg")
+        return logURL!.relativePath
+    }
+    
+    static func stateFile() -> String {
+        let fileManager = FileManager.default
+        let groupURL = fileManager.containerURL(forSecurityApplicationGroupIdentifier: "group.io.netbird.app")
+        let logURL = groupURL?.appendingPathComponent("state.json")
         return logURL!.relativePath
     }
     
