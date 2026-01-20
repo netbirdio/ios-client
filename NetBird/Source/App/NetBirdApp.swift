200b21a2acf8bfd6a6bdecdd458cbed10fd8e467
Diego Romar <doromaraujo+github@gmail.com>
2026-01-20 15:29:54 -0300

    Revert ViewModel creation to synchronous pattern
    Since the slow configureManager() call was removed from
    NetworkExtensionAdapter.init() and the remaining operations are
    lightweight, this asynchronous pattern isn't necessary anymore.
    
    Reason for this commit: app is stuck on netbird logo display on
    cold starts. This was corrected but reintroduced when this
    ViewModelLoader was introduced.

--- NetBird/Source/App/NetBirdApp.swift
+++ NetBird/Source/App/NetBirdApp.swift
@@ -10,7 +10,6 @@
 
 import SwiftUI
 import FirebaseCore
-import Combine
 
 // Firebase Performance is only available on iOS
 #if os(iOS)
@@ -36,8 +35,7 @@ class AppDelegate: NSObject, UIApplicationDelegate {
 
 @main
 struct NetBirdApp: App {
-    // Create ViewModel on background thread to avoid blocking app launch with Go runtime init
-    @StateObject private var viewModelLoader = ViewModelLoader()
+    @StateObject var viewModel = ViewModel()
     @Environment(\.scenePhase) var scenePhase
 
     #if os(iOS)
@@ -56,64 +54,36 @@ struct NetBirdApp: App {
 
     var body: some Scene {
         WindowGroup {
-            if let viewModel = viewModelLoader.viewModel {
-                MainView()
-                    .environmentObject(viewModel)
-                        #if os(iOS)
-                    .onReceive(NotificationCenter.default.publisher(for: UIApplication.didBecomeActiveNotification)) { _ in
+            MainView()
+                .environmentObject(viewModel)
+                #if os(iOS)
+                .onReceive(NotificationCenter.default.publisher(for: UIApplication.didBecomeActiveNotification)) { _ in
+                    print("App is active!")
+                    viewModel.checkExtensionState()
+                    viewModel.checkLoginRequiredFlag()
+                    viewModel.startPollingDetails()
+                }
+                .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)) { _ in
+                    print("App is inactive!")
+                    viewModel.stopPollingDetails()
+                }
+                #endif
+                #if os(tvOS)
+                // tvOS uses scenePhase changes
+                .onChange(of: scenePhase) { _, newPhase in
+                    switch newPhase {
+                    case .active:
                         print("App is active!")
                         viewModel.checkExtensionState()
-                        viewModel.checkLoginRequiredFlag()
                         viewModel.startPollingDetails()
-                    }
-                    .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)) { _ in
+                    case .inactive, .background:
                         print("App is inactive!")
                         viewModel.stopPollingDetails()
+                    @unknown default:
+                        break
                     }
-                    #endif
-                    #if os(tvOS)
-                    // tvOS uses scenePhase changes
-                    .onChange(of: scenePhase) { _, newPhase in
-                        switch newPhase {
-                        case .active:
-                            print("App is active!")
-                            viewModel.checkExtensionState()
-                            viewModel.startPollingDetails()
-                        case .inactive, .background:
-                            print("App is inactive!")
-                            viewModel.stopPollingDetails()
-                        @unknown default:
-                            break
-                        }
-                    }
-                    #endif
-            } else {
-                // Show loading screen while ViewModel initializes
-                Color.black
-                    .ignoresSafeArea()
-                    .overlay(
-                        Image("netbird-logo-menu")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 300)
-                    )
-            }
-        }
-    }
-}
-
-/// Loads ViewModel asynchronously to avoid blocking app launch.
-/// The Go runtime initialization (from NetBirdSDK) can take 10+ seconds on first launch.
-@MainActor
-class ViewModelLoader: ObservableObject {
-    @Published var viewModel: ViewModel?
-
-    init() {
-        // Create ViewModel asynchronously on main thread
-        // The ViewModel itself must be created on MainActor since it's an ObservableObject
-        Task { @MainActor in
-            let vm = ViewModel()
-            self.viewModel = vm
+                }
+                #endif
         }
     }
 }
